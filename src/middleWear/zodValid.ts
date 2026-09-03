0b7c97d2bce8947a677b9a60553f49bdcb7e0575
polokrf <polokkumar9030@gmail.com>
2026-09-03 20:50:07 +0600

    impliment zod valid middlerWear fun

--- src/middleWear/zodValid.ts
+++ src/middleWear/zodValid.ts
@@ -1,3 +1,28 @@
-const zodValid = () => {
-  
+import { NextFunction, Request, Response } from "express"
+import catchAsync from "../utils/catchAsync"
+import * as z from 'zod'
+
+const zodValid = (payload:z.ZodObject) => {
+  return catchAsync(async (req:Request, res:Response, next:NextFunction) => {
+    try {
+      const validPayload = payload.safeParse(req.body);
+
+      if (!validPayload.success) {
+        throw new Error(validPayload.error.issues[0]?.message);
+      }
+
+      req.body = validPayload.data;
+
+      next()
+      
+    } catch (error:any) {
+      console.log(error.message)
+    }
+    
+    
+
+  })
 }
+
+
+export default zodValid
