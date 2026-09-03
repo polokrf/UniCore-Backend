9a00673ee5b788c4ea82a5a07b226a90735c33ef
polokrf <polokkumar9030@gmail.com>
2026-09-03 20:40:46 +0600

    impliment higer oder fun catchAsync

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, RequestHandler, Response } from "express"
+
+const catchAsync = (fu:RequestHandler) => {
+  return async (req:Request, res:Response, next:NextFunction) => {
+    try {
+      await fu(req,res,next)
+    } catch (error) {
+      next()
+    }
+  }
+}
+
+
+export default catchAsync
